811901f836ebe38a0772e0a2140194e640a918ee
alan <alan.paulo@radixeng.com.br>
2021-01-17 01:41:51 -0300

    Put delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -30,7 +30,7 @@ public class OrderService {
         return list.stream().map(p -> new OrderDTO(p)).collect(Collectors.toList());
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public OrderDTO insert(OrderDTO dto) {
         Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
         for (ProductDTO p : dto.getProducts()) {
@@ -38,6 +38,14 @@ public class OrderService {
             order.getProducts().add(product);
         }
 
+        order = repository.save(order);
+        return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO setDelivered(Long id) {
+        Order order = repository.getOne(id);
+        order.setStatus(OrderStatus.DELIVERED);
         order = repository.save(order);
         return new OrderDTO(order);
     }
