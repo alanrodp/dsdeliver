5aee3eb5b4fe72fa63bb343f1551082bf4ae9768
alan <alan.paulo@radixeng.com.br>
2021-01-17 23:51:43 -0300

    Add order list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,11 +1,20 @@
-import { OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 export default function Home() {
-  return (
+
+    const navigation = useNavigation();
+
+    const handleOnPress = () => {
+        navigation.navigate('Orders');
+    }
+
+    return (
     <>
+        <Header/>
         <View style={styles.container}>
             <Image source={require('../assets/deliveryman.png')}/>
             <Text style={styles.title}>
@@ -16,14 +25,14 @@ export default function Home() {
             </Text>
         </View>
         <View style={styles.footer}>
-            <RectButton style={styles.button}>
+            <RectButton style={styles.button} onPress={handleOnPress}>
                 <Text style={styles.buttonText}>
                     VER PEDIDOS
                 </Text>
             </RectButton>
         </View>
     </>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
