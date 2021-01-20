0f92f7e424cc257ca7fa8334284967ff029425b9
alan <alan.paulo@radixeng.com.br>
2021-01-20 00:48:04 -0300

    Add routes and StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { ReactComponent as Main } from './main.svg';
 import Footer from "../Footer";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
@@ -10,13 +11,13 @@ export default function Home() {
                     <div className='home-actions'>
                         <h1 className='home-title'>
                             Faça seu pedido <br /> que entregamos <br /> pra você!!!
-                    </h1>
+                        </h1>
                         <h3 className='home-subtitle'>
                             Escolha o seu pedido e em poucos minutos <br /> levaremoss na sua porta
-                    </h3>
-                        <a className='home-btn-order'>
+                        </h3>
+                        <Link to='/Orders' className='home-btn-order'>
                             FAZER PEDIDO
-                    </a>
+                        </Link>
                     </div>
                     <div className='home-image'>
                         <Main />
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { ReactComponent as Logo } from './logo.svg';
+import StepsHeader from "./StepsHeader";
+
+export default function Orders() {
+    return(
+        <div>
+            <StepsHeader/>
+        </div>
+    )
+}
